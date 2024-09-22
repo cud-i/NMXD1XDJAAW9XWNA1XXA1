14293596ffb1b20758e223a37d26b4e43d0133c1
skibidimaster764 <restrict@gmx.com>
2024-09-22 23:07:08 +0100

    Update and rename test.html to test.php

---
+++ test.php
@@ -0,0 +1,72 @@
+<?php
+// restrict.php
+?>
+
+<?php
+$html = '<!DOCTYPE html>';
+$html .= '<html>';
+$html .= '<head>';
+$html .= '<meta http-equiv="content-type" content="text/html; charset=UTF-8">';
+$html .= '<meta charset="utf-8">';
+$html .= '<title>᲼</title>';
+$html .= '<meta name="viewport" content="width=device-width, initial-scale=1">';
+$html .= '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+$html .= '<link href="https://fonts.cdnfonts.com/css/pf-tempesta-five" rel="stylesheet">';
+$html .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">';
+$html .= '<link rel="stylesheet" href="assets/css/restrict.css" />';
+$html .= '<link rel="stylesheet" href="assets/css/xxx.css" />';
+$html .= '<link rel="shortcut icon" type="image/png" href="assets/media/invis.png" />';
+$html .= '</head>';
+
+$html .= '<body>';
+$html .= '<div id="enter">';
+$html .= '<b class="begin">click to enter</b>';
+$html .= '</div>';
+$html .= '<div id="main">';
+$html .= '<div class="profile-container">';
+$html .= '<div class="profile-picture"></div>';
+$html .= '<div class="name">restrict</div>';
+$html .= '<div class="description"></div>';
+$html .= '<div class="social">';
+$html .= '<a href=\'https://discord.com/users/1154829318569082901\' target=\'_blank\'>';
+$html .= '<img src=\'assets/media/dc.svg\'>';
+$html .= '</a>';
+$html .= '</div>';
+$html .= '<div class="line"></div>';
+$html .= '<div class="info-box">';
+$html .= '<div class="song-info">';
+$html .= '<span class="song-title">OsamaSon - Dirty Bands</span>';
+$html .= '</div>';
+$html .= '<div class="player-controls">';
+$html .= '<div class="music-slider">';
+$html .= '<div class="slider-progress"></div>';
+$html .= '</div>';
+$html .= '<div class="control-buttons">';
+$html .= '<div class="backward-button">';
+$html .= '<img src="assets/media/forward.svg" alt="Backward">';
+$html .= '</div>';
+$html .= '<div class="current-time">0:00</div>';
+$html .= '<div class="pause-button">';
+$html .= '<img src="assets/media/pause.svg" alt="Pause" class="pause-icon">';
+$html .= '<img src="assets/media/play.svg" alt="Play" class="play-icon" style="display: none;">';
+$html .= '</div>';
+$html .= '<div class="forward-button">';
+$html .= '<img src="assets/media/backwards.svg" alt="Forward">';
+$html .= '</div>';
+$html .= '<div class="total-duration">1:34</div>';
+$html .= '</div>';
+$html .= '</div>';
+$html .= '</div>';
+$html .= '<audio id="audio" src="assets/media/aa.mp3"></audio>';
+$html .= '<video id="video" autoplay muted loop>';
+$html .= '<source src="assets/media/aa.mp4" type="video/mp4">';
+$html .= '</video>';
+$html .= '<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>';
+$html .= '<script src="assets/js/resx.js"></script>';
+$html .= '<script src="assets/js/resaudio.js"></script>';
+$html .= '<script src="assets/js/bat.js"></script>';
+$html .= '</body>';
+$html .= '</html>';
+
+echo $html;
+?>
